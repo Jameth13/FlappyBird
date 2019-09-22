316564881fef73bb59da48f9a7ce0aa341307733
James Paton <Jameth13@gmail.com>
2019-09-22 22:22:44 +0100

    Bird object

--- docs/FlappyBird.js
+++ docs/FlappyBird.js
@@ -1,37 +1,68 @@
+//Canvas and context
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
-var bird = new Image();
+//Sprites
 var pipe = new Image();
 var background = new Image();
-bird.src = "sprites/bird.png";
 pipe.src = "sprites/pipe.png";
 background.src = "sprites/background.png";
 
-var birdPos = 300; 
-var velocity = 0.0;
+//Global variables
 var gravity = 20;
 
-var birdOffset = 40;
+//Pipe (TEMP)
 var pipePos = 300;
 var pipeHeight = 800;
 var pipeVelocity = 300;
 var pipeOffset = 1200;
+
+//Score
 var score = 0;
 var highscore = 0;
 
+//deltaTime
 var date = new Date();
 var lastTime = date.getTime();
 var deltaTime = (date.getTime() - lastTime) / 1000;
 
-document.addEventListener("keydown",Jump);
-document.addEventListener("click",Jump);
+//Bird
+var bird = new Bird();
 
+//Input
+document.addEventListener("keydown", Jump);
+document.addEventListener("click", Jump);
+
+//Start game
 Loop();
 
 
+//Bird constructor
+function Bird() {
+    this.x = 40;
+    this.y = 300;
+    this.velocity = 0;
+    this.dead = false;
+    this.sprite = new Image();
+    this.sprite.src = "sprites/bird.png";
+
+    this.Update = function() {
+      this.velocity += gravity * deltaTime;
+      this.y += this.velocity;
+    }
+
+    //HELP!!!
+    this.Jump = function() {
+        if (!this.dead)
+            this.velocity = -10;
+    }
+}
+
+
+//Main loop
 function Loop()
 {
+    //deltaTime
     date = new Date();
     deltaTime = (date.getTime() - lastTime) / 1000;
     lastTime = date.getTime();
@@ -42,10 +73,11 @@ function Loop()
 }
 
 
+//Game logic
 function Game() {
 
-    velocity += gravity * deltaTime;
-    birdPos += velocity;
+    bird.Update();
+
     pipePos -= pipeVelocity * deltaTime;
 
     if (pipePos < -200)
@@ -58,44 +90,54 @@ function Game() {
             highscore = score;
     }
 
-    if (birdPos > canvas.height)
-        Kill();
+    if (bird.y > canvas.height)
+        Reset();
 }
 
 
+//Render
 function Draw() {
+    //Clear canvas
 	context.clearRect(0, 0, canvas.width, canvas.height);
 
-    var birdHitYT = birdPos;
-    var birdHitYB = birdPos + bird.height;
-    var birdHitXR = birdOffset + bird.width;
-    var birdHitXL = birdOffset;
+    //Collision detection
+    var birdHitYT = bird.y;
+    var birdHitYB = bird.y + bird.sprite.height;
+    var birdHitXR = bird.x + bird.sprite.width;
+    var birdHitXL = bird.x;
     var pipeBottom = pipeHeight - pipeOffset + pipe.height;
 
-    if (birdHitYT <= pipeBottom && birdHitXR > pipePos && birdHitXL < pipePos + pipe.width)
-        Kill();
+    if (!bird.dead)
+        if (birdHitXR > pipePos && birdHitXL < pipePos + pipe.width)
+            if (birdHitYT <= pipeBottom || birdHitYB >= pipeHeight)
+                Kill();
 
-    if (birdHitYB >= pipeHeight && birdHitXR > pipePos && birdHitXL < pipePos + pipe.width)
-        Kill();
-
+    //Draw sprites
     context.drawImage(background,0,0);
-    context.drawImage(pipe,pipePos,pipeHeight);
-    context.drawImage(pipe,pipePos,pipeHeight - pipeOffset);
-    context.drawImage(bird,birdOffset,birdPos);
+    context.drawImage(pipe, pipePos, pipeHeight);
+    context.drawImage(pipe, pipePos, pipeHeight - pipeOffset);
+    context.drawImage(bird.sprite, bird.x, bird.y);
 
+    //Draw text
     context.font = "20px Verdana";
-    context.fillText("Score: " + score,4,canvas.clientHeight - 28);
-    context.fillText("Highscore: " + highscore,4,canvas.clientHeight - 8);
+    context.fillText("Score: " + score, 4, canvas.clientHeight - 28);
+    context.fillText("Highscore: " + highscore, 4, canvas.clientHeight - 8);
+    context.fillText("FPS: " + (1 / deltaTime).toFixed(2), 4, 24);
 }
 
 
+//Bird functions - HELP!
 function Jump() {
-	velocity = -10;
+    if (!bird.dead)
+    	bird.velocity = -10;
 }
-
-
 function Kill() {
-    birdPos = pipeHeight - 300;
-    velocity = 0;
+    bird.dead = true;
+    bird.velocity = 8;
+}
+function Reset() {
+    bird.dead = false;
     score = 0;
+    bird.y = pipeHeight - 300;
+    bird.velocity = 0;
 }
